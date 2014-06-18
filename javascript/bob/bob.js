ed955528ad77d344b6ef18c038bc99bef3760b75
Britney Lyons <blyons333@gmail.com>
2014-06-17 21:23:20 -0500

    Updated bob javascript challenge to second version, which is much shorter using regexes and stuff

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,50 +1,14 @@
 var Bob = function() {}
 	Bob.prototype.hey =  function(input) {
-		var last_char = input[input.length - 1];
 		var response = "Whatever.";
-		var upper_count = 0;
-		var letter_count = 0;
-		var letter_and_number_count = 0;
-		for (var i = 0; i < input.length; i++){
-			if (/[A-Za-z]/.exec(input[i])){
-				letter_count++;
-				if (input[i] === input[i].toUpperCase()){
-					upper_count++;
-				}
-			}
-			if (input[i] !== " "){
-				letter_and_number_count++;
-			}
-			
+		if (/[A-Z]/.exec(input) && 
+			!/[a-z]/.exec(input)){
+			response = "Woah, chill out!";
 		}
-		switch(last_char) {
-			case '?':
-				if (upper_count === letter_count 
-					&& upper_count !== 0){
-					response = "Woah, chill out!";
-				}else{
-					response = "Sure.";
-				}
-				break;
-			case '.':
-				if (upper_count === letter_count && 
-					upper_count !== 0){
-					response = "Woah, chill out!";
-				}
-				break;
-			case '!':
-				if (upper_count === letter_count){
-					response = "Woah, chill out!";
-				}
-				break;
-			default:
-				if (upper_count === letter_count && 
-					upper_count !== 0){
-					response = "Woah, chill out!";
-				}
-
+		else if (/\?+$/.exec(input)) {
+			response = "Sure.";
 		}
-		if (letter_and_number_count == 0){
+		else if (input.trim() == ""){
 			response = "Fine. Be that way!";
 		}
 
