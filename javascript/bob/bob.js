854f85ee96de4055579882d46b3e969f6b08e9c1
Britney Lyons <blyons333@gmail.com>
2014-06-19 21:46:33 -0500

    First iteration of rna transcription ruby exercise

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,18 +1,18 @@
 var Bob = function() {}
-	Bob.prototype.hey =  function(input) {
-		var response = "Whatever.";
-		if (/[A-Z]/.exec(input) && 
-			!/[a-z]/.exec(input)){
-			response = "Woah, chill out!";
-		}
-		else if (/\?+$/.exec(input)) {
-			response = "Sure.";
-		}
-		else if (input.trim() == ""){
-			response = "Fine. Be that way!";
-		}
-
-		return response;
+Bob.prototype.hey =  function(input) {
+	var response = "Whatever.";
+	if (/[A-Z]/.exec(input) && 
+		!/[a-z]/.exec(input)){
+		response = "Woah, chill out!";
 	}
+	else if (/\?+$/.exec(input)) {
+		response = "Sure.";
+	}
+	else if (input.trim() == ""){
+		response = "Fine. Be that way!";
+	}
+
+	return response;
+}
 
 module.exports = Bob;
