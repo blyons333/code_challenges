e3ebf7cd4e009265fce5cfdc55d6cb0841ff5417
Britney Lyons <blyons333@gmail.com>
2014-06-19 21:49:05 -0500

    second iteration of rna transcription ruby exercise

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -3,7 +3,7 @@ class Complement
 
 	def self.of_dna(nucleotide)
 		complement_string = ''
-		nucleotide.split('').each{|char|
+		nucleotide.each_char {|char|
 			complement_string << DNA_COMPLEMENTS[char.to_sym]
 		}
 		return complement_string
@@ -11,7 +11,7 @@ class Complement
 
 	def self.of_rna(nucleotide)
 		complement_string = ''
-		nucleotide.split('').each {|char|
+		nucleotide.each_char {|char|
 			complement_string << DNA_COMPLEMENTS.key(char).to_s
 		}
 		return complement_string
