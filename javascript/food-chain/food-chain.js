ecf0c12b4600afa445d5638623e5075ff81ea429
Britney Lyons <blyons333@gmail.com>
2014-12-14 12:40:10 -0600

    updated the food chain javascript exercise so that the song class can take in any set of lyrics and split it into verses

--- javascript/food-chain/food-chain.js
+++ javascript/food-chain/food-chain.js
@@ -1,56 +1,4 @@
-var song = function(){
-	//The song as one big, long string. This was super annoying
-	//to add to this file...
-	var songString = "I know an old lady who swallowed a fly.\n\
-I don't know why she swallowed the fly. Perhaps she'll die.\n\
-\n\
-I know an old lady who swallowed a spider.\n\
-It wriggled and jiggled and tickled inside her.\n\
-She swallowed the spider to catch the fly.\n\
-I don't know why she swallowed the fly. Perhaps she'll die.\n\
-\n\
-I know an old lady who swallowed a bird.\n\
-How absurd to swallow a bird!\n\
-She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n\
-She swallowed the spider to catch the fly.\n\
-I don't know why she swallowed the fly. Perhaps she'll die.\n\
-\n\
-I know an old lady who swallowed a cat.\n\
-Imagine that, to swallow a cat!\n\
-She swallowed the cat to catch the bird.\n\
-She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n\
-She swallowed the spider to catch the fly.\n\
-I don't know why she swallowed the fly. Perhaps she'll die.\n\
-\n\
-I know an old lady who swallowed a dog.\n\
-What a hog, to swallow a dog!\n\
-She swallowed the dog to catch the cat.\n\
-She swallowed the cat to catch the bird.\n\
-She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n\
-She swallowed the spider to catch the fly.\n\
-I don't know why she swallowed the fly. Perhaps she'll die.\n\
-\n\
-I know an old lady who swallowed a goat.\n\
-Just opened her throat and swallowed a goat!\n\
-She swallowed the goat to catch the dog.\n\
-She swallowed the dog to catch the cat.\n\
-She swallowed the cat to catch the bird.\n\
-She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n\
-She swallowed the spider to catch the fly.\n\
-I don't know why she swallowed the fly. Perhaps she'll die.\n\
-\n\
-I know an old lady who swallowed a cow.\n\
-I don't know how she swallowed a cow!\n\
-She swallowed the cow to catch the goat.\n\
-She swallowed the goat to catch the dog.\n\
-She swallowed the dog to catch the cat.\n\
-She swallowed the cat to catch the bird.\n\
-She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n\
-She swallowed the spider to catch the fly.\n\
-I don't know why she swallowed the fly. Perhaps she'll die.\n\
-\n\
-I know an old lady who swallowed a horse.\n\
-She's dead, of course!\n";
+var song = function(songString){
 
 	//Instantiate the new song array
 	this.songArray = new Array();
@@ -122,4 +70,58 @@ song.prototype.verses = function(verseBegin, verseEnd){
 	return combinedVerses;
 }
 
-module.exports = new song();
+//The song as one big, long string. This was super annoying
+//to add to this file...
+var songString = "I know an old lady who swallowed a fly.\n\
+I don't know why she swallowed the fly. Perhaps she'll die.\n\
+\n\
+I know an old lady who swallowed a spider.\n\
+It wriggled and jiggled and tickled inside her.\n\
+She swallowed the spider to catch the fly.\n\
+I don't know why she swallowed the fly. Perhaps she'll die.\n\
+\n\
+I know an old lady who swallowed a bird.\n\
+How absurd to swallow a bird!\n\
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n\
+She swallowed the spider to catch the fly.\n\
+I don't know why she swallowed the fly. Perhaps she'll die.\n\
+\n\
+I know an old lady who swallowed a cat.\n\
+Imagine that, to swallow a cat!\n\
+She swallowed the cat to catch the bird.\n\
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n\
+She swallowed the spider to catch the fly.\n\
+I don't know why she swallowed the fly. Perhaps she'll die.\n\
+\n\
+I know an old lady who swallowed a dog.\n\
+What a hog, to swallow a dog!\n\
+She swallowed the dog to catch the cat.\n\
+She swallowed the cat to catch the bird.\n\
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n\
+She swallowed the spider to catch the fly.\n\
+I don't know why she swallowed the fly. Perhaps she'll die.\n\
+\n\
+I know an old lady who swallowed a goat.\n\
+Just opened her throat and swallowed a goat!\n\
+She swallowed the goat to catch the dog.\n\
+She swallowed the dog to catch the cat.\n\
+She swallowed the cat to catch the bird.\n\
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n\
+She swallowed the spider to catch the fly.\n\
+I don't know why she swallowed the fly. Perhaps she'll die.\n\
+\n\
+I know an old lady who swallowed a cow.\n\
+I don't know how she swallowed a cow!\n\
+She swallowed the cow to catch the goat.\n\
+She swallowed the goat to catch the dog.\n\
+She swallowed the dog to catch the cat.\n\
+She swallowed the cat to catch the bird.\n\
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n\
+She swallowed the spider to catch the fly.\n\
+I don't know why she swallowed the fly. Perhaps she'll die.\n\
+\n\
+I know an old lady who swallowed a horse.\n\
+She's dead, of course!\n";
+
+
+module.exports = new song(songString);
