8949c1d741ba82eec7fe5de8164839389480e0aa
Britney Lyons <blyons333@gmail.com>
2014-12-14 12:31:10 -0600

    second round of python bob exercise. Much more condensed and

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -7,51 +7,14 @@ class Bob:
 
 	def hey(self, what):
 		rtnResponse = "Whatever."
+		what = what.strip()
 
-		if self._isWhitespace(what):
+		if len(what) == 0:
 			rtnResponse = "Fine. Be that way!"
-		elif self._isQuestion(what):
-			rtnResponse = "Sure."
-		elif self._isYelling(what):
+		elif what.isupper():
 			rtnResponse = "Woah, chill out!"
+		elif what.endswith("?"):
+			rtnResponse = "Sure."
 
 		return rtnResponse
 
-	def _isWhitespace(self, what):
-		rtnVal = len(what.strip()) == 0
-		return rtnVal
-
-	def _isQuestion(self, what):
-		rtnVal = False
-		lastChar = self._getLastCharacter(what)
-		
-		if lastChar == "?" and not self._isAllUppercase(what):
-			rtnVal = True
-
-		return rtnVal
-
-	def _isYelling(self, what):
-		rtnVal = False
-
-		if self._isAllUppercase(what):
-			rtnVal = True
-
-		return rtnVal
-
-	def _getLastCharacter(self, what):
-		lastChar = what[len(what)-1]
-		return lastChar
-
-	def _isAllUppercase(self, what):
-		#remove any characters from the string that aren't
-		#letters to test if they're all uppercase
-		test_string = filter(lambda c: c.isalpha(), what)
-		
-		if len(test_string) == 0:
-			return False
-
-		for c in test_string:
-			if c.islower():
-				return False
-
-		return True
